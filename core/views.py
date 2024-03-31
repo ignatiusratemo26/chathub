9dc96949100d88a8e2ee7cc773be1b357df99290
ignatiusiggy5 <ignatiusratemo26@gmail.com>
2024-04-01 00:31:12 +0300

    Sign up page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login
+from .forms import SignUpForm
 
-# Create your views here.
 def frontpage(request):
-    return render(request, 'core/base.html')
+    return render(request, 'core/index.html')
+
+#creating the sign up view
+def signup(request):
+    #to check if form is submitted
+    if request.method == 'POST':
+        #new instance of the form
+        form = SignUpForm(request.POST)
+        #to check if credentials are okay
+        if form.is_valid():
+            #save user if credentials are okay
+            user = form.save()
+            login(request, user)
+
+            return redirect('frontpage')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {'form': form})
+
+def login(request):
+    return
